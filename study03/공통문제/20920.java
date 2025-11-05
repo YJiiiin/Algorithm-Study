ff051bba7ac7bb8cad3d07293342d8dedb1b6e28
YJiiiin <97524298+YJiiiin@users.noreply.github.com>
2025-11-06 05:28:18 +0900

    [백준] 영단어 암기는 어려워

--- study03/공통문제/20920.java
+++ study03/공통문제/20920.java
@@ -9,9 +9,9 @@ public class Main {
         StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken()); 
         int M = Integer.parseInt(st.nextToken()); 
-        
+
+        //단어 길이가 M이상일 때 저장
         HashMap<String, Integer> wordCountMap = new HashMap<>();
-        
         for (int i = 0; i < N; i++) {
             String word = br.readLine();
             if (word.length() >= M) {
@@ -19,23 +19,24 @@ public class Main {
             }
         }
 
+        //단어 출력 위해 List 변환
         List<String> words = new ArrayList<>(wordCountMap.keySet());
-        
+
+        //정렬
         Collections.sort(words, (word1, word2) -> {
             int count1 = wordCountMap.get(word1);
             int count2 = wordCountMap.get(word2);
-            if (count1 != count2) return count2 - count1;
-            if (word1.length() != word2.length()) return word2.length() - word1.length();
-            return word1.compareTo(word2);
+            if (count1 != count2) return count2 - count1;    //1. 등장 횟수 내림차순
+            if (word1.length() != word2.length()) return word2.length() - word1.length();    //단어 길이 내림차순
+            return word1.compareTo(word2);    //3. 알파벳 순서 오름차순
         });
 
         for (String word : words) {
             bw.write(word + "\n");
         }
         
-        bw.flush();
+        bw.flush();    //BufferedWriter로 출력 후 flush 마무리
         bw.close();
         br.close();
     }
 }
-
