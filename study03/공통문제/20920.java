91f051bc0bc91819effcf11bee657721e8815254
YJiiiin <97524298+YJiiiin@users.noreply.github.com>
2025-11-06 12:57:04 +0900

    [백준] 영단어 암기는 어려워

--- study03/공통문제/20920.java
+++ study03/공통문제/20920.java
@@ -3,12 +3,13 @@ import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
+        //빠른 입출력
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken()); 
-        int M = Integer.parseInt(st.nextToken()); 
+        int N = Integer.parseInt(st.nextToken()); //단어 개수
+        int M = Integer.parseInt(st.nextToken()); //외워야 할 단어 최소 길
 
         //단어 길이가 M이상일 때 저장
         HashMap<String, Integer> wordCountMap = new HashMap<>();
@@ -27,16 +28,18 @@ public class Main {
             int count1 = wordCountMap.get(word1);
             int count2 = wordCountMap.get(word2);
             if (count1 != count2) return count2 - count1;    //1. 등장 횟수 내림차순
-            if (word1.length() != word2.length()) return word2.length() - word1.length();    //단어 길이 내림차순
+            if (word1.length() != word2.length()) return word2.length() - word1.length();    //2. 단어 길이 내림차순
             return word1.compareTo(word2);    //3. 알파벳 순서 오름차순
         });
 
+        //BufferedWriter로 출력 후 flush 마무리
         for (String word : words) {
             bw.write(word + "\n");
         }
         
-        bw.flush();    //BufferedWriter로 출력 후 flush 마무리
+        bw.flush();    
         bw.close();
         br.close();
     }
 }
+
