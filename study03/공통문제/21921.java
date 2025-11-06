87e29aedbbab0b74696ac5ec4de6a34a6ff7aef8
YJiiiin <97524298+YJiiiin@users.noreply.github.com>
2025-11-06 12:53:04 +0900

    [백준] 블

--- study03/공통문제/21921.java
+++ study03/공통문제/21921.java
@@ -3,7 +3,7 @@ import java.util.*;
 public class Main{
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
-		int N = scan.nextInt(); //지난 일수
+		int N = scan.nextInt(); //블로그 시작하고 지난 일수
 		int X = scan.nextInt(); //가장 많이 들어온 방문자 수를 구해야 할 일수
 
 		//N일 동안 하루 방문자 수
@@ -22,7 +22,8 @@ public class Main{
 		
 		for(int i=X; i<N; i++) {
 			sum = sum - arr[i-X] + arr[i]; //맨 첫번째 항은 빼주고 새로운 항을 더해준다.
-		
+
+			//최대 방문자 수
 			if(sum>max) {
 				max = sum;
 				count = 1;
@@ -31,7 +32,8 @@ public class Main{
 				count++;
 			}
 		}
-		
+
+		//출력
 		if(max==0) {
 			System.out.println("SAD");
 		}
@@ -42,4 +44,5 @@ public class Main{
 		
 	}
 }
+
 
