c927ae062b1b4b1d4e3b88471ba0a75d4dcbfbab
YJiiiin <97524298+YJiiiin@users.noreply.github.com>
2025-11-06 12:51:44 +0900

    [백준] 블

--- study03/공통문제/21921.java
+++ study03/공통문제/21921.java
@@ -4,19 +4,21 @@ public class Main{
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		int N = scan.nextInt(); //지난 일수
-		int X = scan.nextInt(); 
-		
+		int X = scan.nextInt(); //가장 많이 들어온 방문자 수를 구해야 할 일수
+
+		//N일 동안 하루 방문자 수
 		int[] arr = new int[N];
 		for(int i=0; i<N; i++) {
 			arr[i] = scan.nextInt();
 		}
-		
+
+		//첫날~X일까지의 방문자 수 합
 		int sum = 0;
 		for(int i=0; i<X; i++) {
 			sum += arr[i];
 		}
 		int max = sum; //최댓값 설정
-		int count = 1; //개수
+		int count = 1; //개수(최대 방문자 수가 갈은 기간의 개수)
 		
 		for(int i=X; i<N; i++) {
 			sum = sum - arr[i-X] + arr[i]; //맨 첫번째 항은 빼주고 새로운 항을 더해준다.
@@ -40,3 +42,4 @@ public class Main{
 		
 	}
 }
+
